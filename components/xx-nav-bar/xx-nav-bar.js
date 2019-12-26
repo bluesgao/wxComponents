a5aa100329b35adaf6f2e2ff630d045d9d6a919f
bluesgao <bluesgao@126.com>
2019-12-26 15:35:33 +0800

    navbar

---
+++ components/xx-nav-bar/xx-nav-bar.js
@@ -0,0 +1,34 @@
+Component({
+    data: {},
+    properties: {
+        needBack: {
+            type: Boolean,
+            value: false
+        },
+        needHome: {
+            type: Boolean,
+            value: false
+        },
+        needSearch: {
+            type: Boolean,
+            value: false
+        },
+        needTab: {
+            type: Boolean,
+            value: false
+        },
+        title: {
+            type: String,
+            value: ''
+        },
+        iconTheme: {
+            type: String,
+            value: "black"
+        },
+        background: {
+            type: String,
+            value: ''
+        }
+    },
+    methods: {}
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,12 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    //顶部导航栏设置
+    navBarCfg:{
+      needHome:true,
+      needBack: true
+    }
   },
   //事件处理函数
   bindViewTap: function() {
