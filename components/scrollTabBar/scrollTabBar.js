1d63f60eeb6c4bd8e8a009f20a4a316961f0dd1c
bluesgao <bluesgao@126.com>
2019-12-30 16:12:29 +0800

    scrollTabBar

--- components/scrollTabBar/scrollTabBar.js
+++ components/scrollTabBar/scrollTabBar.js
@@ -1,5 +1,34 @@
 Component({
-  data: {},
-  properties: {},
-  methods: {}
+  data: {
+    tabList: ['今', '烟', '净'],
+    current: 0
+  },
+  properties: {
+    tabList: {
+      type: Object,
+      value: []
+    },
+    currentTab: {
+      type: Number,
+      value: 0
+    }
+  },
+  attached: function () {
+    console.info('attached ', this.data)
+
+
+
+  },
+  methods: {
+    //选择tab
+    selectTab: function (e) {
+      console.info('selectTab ', e)
+      console.info('selectTab currentTarget', e.currentTarget.dataset.index)
+      this.setData({
+        currentTab: e.currentTarget.dataset.index,
+      })
+      console.info('selectTab data ', this.data)
+
+    },
+  }
 })
